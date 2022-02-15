aa4096f2bc68c4b40617457bb48828f7007f03d3
Anupam Dubey <9430137649mk@gmail.com>
2022-02-15 23:18:34 +0530

    Fixed middleware import order

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const noteRouter = require('./routes/Notes');
 
 app.use(cors());
 app.use(express.json()); 
+app.use('/', noteRouter);
 
 // Connect to MongoDB
 const uri = process.env.ATLAS_URI;
@@ -18,10 +20,6 @@ const connection = mongoose.connection;
 connection.once('open',  () => {
     console.log('MongoDB connection established');
 })
-// Require the noteSchema
-const noteRouter = require('./routes/Notes');
-
-app.use('/', noteRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
